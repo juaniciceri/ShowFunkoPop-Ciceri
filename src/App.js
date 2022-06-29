46b2b3d71afcb446ffa9b7097ff4f97a2ce49c19
carlos ciceri <juanncice@gmail.com>
2022-06-29 15:45:20 -0300

    navbar con bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import * as ReactBootstrap from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactBootstrap.Navbar bg="primary" expand="xl">
+  <ReactBootstrap.Container>
+    <ReactBootstrap.Navbar.Brand href="#home">FunkoShop</ReactBootstrap.Navbar.Brand>
+    <ReactBootstrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <ReactBootstrap.Navbar.Collapse id="basic-navbar-nav">
+      <ReactBootstrap.Nav className="me-auto">
+        <ReactBootstrap.Nav.Link href="#home">Home</ReactBootstrap.Nav.Link>
+        <ReactBootstrap.Nav.Link href="#link">Contact</ReactBootstrap.Nav.Link>
+        <ReactBootstrap.NavDropdown title="More" id="basic-nav-dropdown">
+          <ReactBootstrap.NavDropdown.Item href="#action/3.1">About us</ReactBootstrap.NavDropdown.Item>
+          <ReactBootstrap.NavDropdown.Item href="#action/3.2">Products</ReactBootstrap.NavDropdown.Item>
+          <ReactBootstrap.NavDropdown.Item href="#action/3.3">Something</ReactBootstrap.NavDropdown.Item>
+          <ReactBootstrap.NavDropdown.Divider />
+          <ReactBootstrap.NavDropdown.Item href="#action/3.4">Separated link</ReactBootstrap.NavDropdown.Item>
+        </ReactBootstrap.NavDropdown>
+      </ReactBootstrap.Nav>
+    </ReactBootstrap.Navbar.Collapse>
+  </ReactBootstrap.Container>
+</ReactBootstrap.Navbar>
     </div>
   );
 }
